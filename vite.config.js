dce44846d245dd7070b9a84b68eeaa293a3e630d
petershale21 <petershale21@gmail.com>
2025-03-06 09:38:39 -0500

    add github pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+    base: "/https://github.com/petershale21/learning_react",
   plugins: [react()],
 })
